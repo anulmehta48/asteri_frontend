0f0a03b76f888d2fe194255b816baf2073d85440
Anul Mehta <anulmehta1848@gmail.com>
2025-03-07 23:52:27 +0530

    7

--- src/components/Body.js
+++ src/components/Body.js
@@ -85,58 +85,64 @@ const Body = () => {
           </div>
           {/* Text div */}
           <div className="lg:flex-2 mt-15 mx-6 justify-center items-center ">
-            <h1 className="text-3xl md:text-5xl md:font-extralight ">
-              Comfort and savings through automation
+            <h1 className="text-4xl md:text-5xl md:font-extralight ">
+              Confort et économies grâce à l'automatisation
             </h1>
             <div className="mt-10 md:gap-10  ">
               {/* Item 1 */}
               <div className="flex items-start  ">
                 <div className="flex flex-col items-center mr-5">
                   <div className="w-0.5 h-5 bg-gray-600 "></div>
-                  <span className="bg-gray-600 text-white text-lg font-bold px-3 py-1 rounded-md   ">
+                  <span className="bg-gray-600 text-white text-lg md:text-2xl  font-bold px-3 py-1 rounded-md   ">
                     1
                   </span>
                   <div className="w-0.5 h-16 bg-gray-600"></div>
+                  <div className="w-0.5 h-16 bg-gray-600"></div>
                 </div>
                 <div className="w-15 h-0.5 mt-10 bg-gray-600"></div>
-                <div className="space-y-4 ml-5 mt-5 ">
-                  <h3 className="text-2xl font-light "> Presence detection</h3>
+                <div className="w-15 h-0.5 mt-10 bg-gray-600"></div>
+                <div className="space-y-4 mt-5 ">
+                  <h3 className="text-2xl font-light ">
+                    {" "}
+                    Détection de présence
+                  </h3>
                   <p className="font-light md:text-wrap md:text-xl  ">
-                    Our sensors automatically turn off the air conditioning when
-                    vacationers go out.
+                    Nos capteurs coupent automatiquement la climatisation quand
+                    les vacanciers sortent.
                   </p>
                 </div>
               </div>
               {/* Item 2 */}
               <div className="flex items-start ">
                 <div className="flex flex-col items-center mr-4">
-                  <span className="bg-gray-600 text-white text-lg font-bold px-3 py-1 rounded-md">
+                  <span className="bg-gray-600 text-white text-lg md:text-2xl font-bold px-3 py-1 rounded-md">
                     2
                   </span>
+                  <div className="w-0.5 h-16 bg-gray-600"></div>
                   <div className="w-0.5 h-16 bg-gray-600"></div>
                 </div>
                 <div className="w-15 h-0.5 mt-5 bg-gray-600"></div>
                 <div className="space-y-4 ">
-                  <h3 className="text-2xl font-light">Real-time tracking</h3>
+                  <h3 className="text-2xl font-light">Suivi en temps réel</h3>
                   <p className="font-light md:text-wrap md:text-xl">
-                    Track consumption data for each home.
+                    Suivez les données de consommation de chaque logement.
                   </p>
                 </div>
               </div>
               {/* Item 3 */}
               <div className="flex items-start ">
                 <div className="flex flex-col items-center mr-4">
-                  <span className="bg-gray-600 text-white text-lg font-bold px-3 py-1 rounded-md">
+                  <span className="bg-gray-600 text-white text-lg md:text-2xl  font-bold px-3 py-1 rounded-md">
                     3
                   </span>
                   <div className="w-0.5 h-5 bg-gray-600 "></div>
                   <div className="w-0.5 h-0 bg-gray-600"></div>
                 </div>
-                <div className="w-15 h-0.5 mt-5 bg-gray-600"></div>
+                <div className="w-15 h-0.5 mt-5  bg-gray-600"></div>
                 <div className="space-y-4 ">
-                  <h3 className="text-2xl font-light">Energy savings</h3>
+                  <h3 className="text-2xl font-light">Économies d'énergie</h3>
                   <p className="font-light md:text-wrap md:text-xl">
-                    Reduce energy consumption by 20 to 30%.
+                    Réduisez la consommation énergétique de 20 à 30%.
                   </p>
                 </div>
               </div>
@@ -145,30 +151,31 @@ const Body = () => {
         </div>
       </div>
       {/* //container-3 */}
-      <div className="bg-white py-16 md:py-25">
+      <div className="bg-gray-100 py-16 md:py-25">
         <div className="text-black max-w-6xl mx-auto px-4">
           <h1 className="text-4xl md:text-5xl mx-7">
-            Simple and reliable solutions
+            Des solutions simples et fiables
           </h1>
           <div className="space-y-10 mt-10 md:flex md:space-x-8 mx-8 md:justify-center">
             <div className="space-y-1.5">
-              <h1 className="font-light text-3xl ">Full warranty</h1>
+              <h1 className="font-light text-3xl ">Garantie complète</h1>
               <p className="font-extralight text-xl text-wrap">
-                Replacement of defective equipment for one year, at no
-                additional cost.
+                Remplacement d'équipement défectueux pendant un an, sans frais
+                supplémentaire.
               </p>
             </div>
             <div className="space-y-1.5">
-              <h1 className="font-light text-3xl ">Insurance included</h1>
+              <h1 className="font-light text-3xl ">Assurance incluse</h1>
               <p className="font-extralight text-xl text-wrap">
-                Your installation is covered, for lasting peace of mind.
+                Votre installation est couverte, pour une tranquillité d'esprit
+                durable.
               </p>
             </div>
             <div className="space-y-1.5">
-              <h1 className="font-light text-3xl ">Scalable subscription</h1>
+              <h1 className="font-light text-3xl ">Abonnement évolutif</h1>
               <p className="font-extralight text-xl text-wrap">
-                {" "}
-                Updates and support included for optimized performance.
+                Mises à jour et assistance inclus pour des performances
+                optimisées.
               </p>
             </div>
           </div>
@@ -176,65 +183,69 @@ const Body = () => {
       </div>
       {/*//container-4 */}
       <div className="bg-black md:py-16">
-        <div className="max-w-6xl mx-auto md:flex">
+        <div className="max-w-6xl mx-auto md:flex md:gap-4">
           <div className="">
             <img
               src={IMG_PROD_CDN_URL}
               alt="toy"
-              className="md:h-[520] md:w-[650] md:rounded-2xl "
+              className="md:h-[620] md:w-[950] md:rounded-xl object-cover"
             />
           </div>
-          <div className="text-white mt-10 mx-10">
+          <div className="text-white mt-10 mx-10 ">
             <h1 className="text-5xl text-wrap ">
-              Advanced technology, immediate results
+              Technologie avancée, résultats immédiats
             </h1>
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-12 mt-8">
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-12 mt-14">
               <div className="flex items-start space-x-3">
-                <span className="bg-gray-600 text-white text-lg font-bold px-3 py-1 rounded-md   ">
+                <span className="bg-gray-600 text-white text-lg md:text-2xl font-bold px-3 py-1 rounded-md   ">
                   1
                 </span>
-                <div>
+                <div className="space-y-4">
                   <h1 className="text-xl font-semibold">
-                    Installation from A to Z
+                    Installation <strong>de A à Z</strong>
                   </h1>
-                  <p className="text-gray-300">
-                    Turnkey offer for the intelligent management of your air
-                    conditioning
+                  <p className="text-gray-300 text-xl">
+                    Offre clé en main pour la gestion intelligente de vos
+                    climatisations
                   </p>
                 </div>
               </div>
               <div className="flex items-start space-x-3">
-                <span className="bg-gray-600 text-white text-lg font-bold px-3 py-1 rounded-md   ">
+                <span className="bg-gray-600 text-white text-lg  md:text-2xl  font-bold px-3 py-1 rounded-md   ">
                   2
                 </span>
-                <div>
-                  <h1 className="text-xl font-semibold">Intuitive dashboard</h1>
-                  <p className="text-gray-300">
-                    Remote control via mobile application
+                <div className="space-y-4">
+                  <h1 className="text-xl font-semibold">
+                    <strong>intuitif</strong> Tableau de bord
+                  </h1>
+                  <p className="text-gray-300 text-xl">
+                    Contrôle à distance via application mobile,
                   </p>
                 </div>
               </div>
               <div className="flex items-start space-x-3">
-                <span className="bg-gray-600 text-white text-lg font-bold px-3 py-1 rounded-md   ">
+                <span className="bg-gray-600 text-white text-lg  md:text-2xl font-bold px-3 py-1 rounded-md   ">
                   3
                 </span>
-                <div>
+                <div className="space-y-4">
                   <h1 className="text-xl font-semibold">
-                    Real-time monitoring
+                    <strong> temps réel</strong> Surveillance en
                   </h1>
-                  <p className="text-gray-300">
-                    Track energy savings in real time.
+                  <p className="text-gray-300 text-xl">
+                    Suivez les économies d'énergie en temps réel.
                   </p>
                 </div>
               </div>
               <div className="flex items-start space-x-3">
-                <span className="bg-gray-600 text-white text-lg font-bold px-3 py-1 rounded-md   ">
+                <span className="bg-gray-600 text-white text-lg  md:text-2xl  font-bold px-3 py-1 rounded-md   ">
                   4
                 </span>
-                <div>
-                  <h1 className="text-xl font-semibold">Detailed reports</h1>
-                  <p className="text-gray-300">
-                    Analyze consumption data to optimize.
+                <div className="space-y-4">
+                  <h1 className="text-xl font-semibold">
+                    <strong>détaillés</strong> Rapports
+                  </h1>
+                  <p className="text-gray-300 text-xl">
+                    Analysez les données de consommation pour optimiser.
                   </p>
                 </div>
               </div>
@@ -242,7 +253,7 @@ const Body = () => {
           </div>
         </div>
       </div>
-
+      {/*//container-5*/}
       <div className="bg-black py-16">
         <div className="text-white max-w-6xl mx-auto px-4">
           <div className="w-full flex justify-center items-center flex-col ">
@@ -256,41 +267,41 @@ const Body = () => {
         </div>
       </div>
 
-      {/*//container-5 */}
+      {/*//container-6 */}
       <div className="bg-white py-5 md:py-16">
         <div className="text-white max-w-6xl mx-2 md:mx-auto md:p-10 bg-black space-y-2.5 rounded-2xl">
           <div className="text-4xl md:text-5xl md:font-normal text-wrap font-medium p-5">
-            <h1>Testimonials from our satisfied customers </h1>
+            <h1>Témoignages de nos clients satisfaits </h1>
           </div>
           <div className="space-y-5 text-wrap p-5 grid grid-cols-1 md:grid-cols-3 md:gap-6">
             <div className="bg-gray-600 rounded-xl p-5 space-y-2 md:h-full">
               <h1 className="text-3xl font-light">Johan</h1>
               <p className="text-xl font-light italic">
-                25% reduction on our bills thanks to these sensors.
+                Réductions de 25% sur nos factures grâce à ces capteurs.
               </p>
             </div>
             <div className="bg-gray-600 rounded-xl p-5 space-y-2 md:h-full">
               <h1 className="text-3xl font-light">Karen</h1>
               <p className="text-xl font-light italic">
-                Return on investment in less than 2 years thanks to cost
-                reductions.
+                Retour sur investissement.en moins de 2 ans grâce aux réductions
+                de coûts.
               </p>
             </div>
             <div className="bg-gray-600 rounded-xl p-5 space-y-2 md:h-full">
               <h1 className="text-3xl font-light">Heusia</h1>
               <p className="text-xl font-light italic">
-                Loyalty of tenants, who really appreciate our ecological
-                commitment
+                Fidélisation des locataires, qui apprécient vraiment notre
+                engagement écologique
               </p>
             </div>
           </div>
         </div>
       </div>
-      {/*//container-6 */}
+      {/*//container-7 */}
       <div className="bg-black py-16">
         <div className="text-white max-w-6xl mx-auto px-4">
           <div className=" text-4xl p-5 md:text-5xl text-wrap">
-            <h1>A profitable and ecological investment</h1>
+            <h1>Un investissement rentable et écologique</h1>
           </div>
           <div className="p-5 text-wrap space-y-10 grid grid-cols-1 md:grid-cols-4 md:gap-5 ">
             <div className=" md:h-full">
@@ -299,8 +310,10 @@ const Body = () => {
                 alt="power"
                 className="h-10 invert brightness-0 mb-4"
               />
-              <h1 className="text-3xl">Savings</h1>
-              <p className="text-2xl font-light">Reduced electricity bills</p>
+              <h1 className="text-3xl">Économies</h1>
+              <p className="text-2xl font-light">
+                Factures d'électricité réduites
+              </p>
             </div>
             <div className="  md:h-full">
               <img
@@ -308,9 +321,9 @@ const Body = () => {
                 alt="sustain"
                 className="h-10 invert brightness-0 mb-4"
               />
-              <h1 className="text-3xl">Sustainability</h1>
+              <h1 className="text-3xl">Durabilité</h1>
               <p className="text-2xl font-light">
-                Reduced environmental footprint
+                Empreinte environnementale diminuée
               </p>
             </div>
             <div className="  md:h-full">
@@ -321,7 +334,7 @@ const Body = () => {
               />
               <h1 className="text-3xl">Satisfaction</h1>
               <p className="text-2xl font-light">
-                Customer expectations met, loyalty strengthened
+                Attentes des clients comblées, fidélité renforcée
               </p>
             </div>
             <div className="  md:h-full">
@@ -330,15 +343,15 @@ const Body = () => {
                 alt="profit"
                 className="h-10 invert brightness-0 mb-4"
               />
-              <h1 className="text-3xl">Profitability</h1>
+              <h1 className="text-3xl">Rentabilité</h1>
               <p className="text-2xl font-light">
-                Return on investment in less than 2 years
+                Retour sur investissement en moins de 2 ans
               </p>
             </div>
           </div>
         </div>
       </div>
-      {/*//container-6 */}
+      {/*//container-8 */}
       <div
         style={{ backgroundImage: `url(${IMG_WAR_CDN_URL})` }}
         className="relative py-16 bg-cover bg-center bg-no-repeat"
@@ -346,7 +359,7 @@ const Body = () => {
         <div className="absolute inset-0 bg-black opacity-50"></div>
         <div className="max-w-4xl mx-auto ">
           <h2 className="relative text-4xl text-white mb-10 mx-5 md:mx-auto">
-            Start your ecological transition now
+            Démarrez maintenant votre transition écologique
           </h2>
           <div className="relative  space-y-5 mx-5 md:mx-auto">
             {/* Step 1 */}
@@ -366,11 +379,13 @@ const Body = () => {
                 </span>
               </div>
               <div className=" text-white space-y-2">
-                <h3 className="text-xl font-semibold">Needs assessment</h3>
+                <h3 className="text-xl font-semibold">
+                  Évaluation des besoins
+                </h3>
                 <p>
-                  Analysis of your real estate portfolio: we carry out an{" "}
-                  <strong>on-site visit</strong> to assess the specific needs of
-                  the accommodation.
+                  Analyse de votre parc immobilier : nous réalisons une{" "}
+                  <strong>visite sur place </strong>pour évaluer les besoins
+                  spécifiques des logements.
                 </p>
               </div>
             </div>
@@ -392,17 +407,17 @@ const Body = () => {
               </div>
               <div className=" text-white space-y-2">
                 <h3 className="text-xl font-semibold">
-                  Sensor installation and scenario definitions
+                  Installation des capteurs et définitions des scenarii
                 </h3>
-                <ul className="list-disc list-inside ">
-                  <li>Quick and discreet installation.</li>
+                <ul className="list-disc ">
+                  <li>Mise en place rapide et discrète.</li>
                   <li>
-                    Programming according to lifestyle habits and configuration
-                    of switch-on and switch-off time slots.
+                    Programmation selon les habitudes de vie et configuration
+                    des plages horaires d’allumage et d’extinction
                   </li>
                   <li>
-                    Integration with other home automation systems (lighting,
-                    security).
+                    Intégration avec d'autres systèmes domotiques (éclairage,
+                    sécurité).
                   </li>
                 </ul>
               </div>
@@ -425,9 +440,9 @@ const Body = () => {
               </div>
               <div className=" text-white space-y-2">
                 <h3 className="text-xl font-semibold">
-                  Continuous optimization of your scenarios
+                  Optimisation continue de vos scenarii
                 </h3>
-                <p>Continuous monitoring and adjustments.</p>
+                <p>Surveillance et ajustements permanents</p>
               </div>
             </div>
           </div>
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,23 +1,69 @@
+import { FaFacebook, FaTwitter, FaLinkedin, FaInstagram } from "react-icons/fa";
 const Footer = () => {
   return (
     <div className="bg-black py-16">
-      <div className="text-white max-w-6xl mx-auto px-4">
+      <div className="text-white max-w-6xl mx-auto px-4 space-y-5 ">
         <div className="space-y-10">
-          <h1 className="text-3xl md:text-5xl">Ready to take the plunge?</h1>
+          <h1 className="text-3xl md:text-5xl">Prêt à franchir le cap ?</h1>
           <div className="space-y-5">
             <div className="flex flex-col gap-2 md:flex-row ">
               <button className="bg-white font-bold rounded-md text-black py-3 border-black md:p-3 cursor-pointer ">
-                <a href="https://forms.gle/5eqSGb5FvqsS6g8z8"> Contact us </a>
+                <a href="https://forms.gle/5eqSGb5FvqsS6g8z8">
+                  {" "}
+                  Contactez nous{" "}
+                </a>
               </button>
 
               <button className="bg-black rounded-md text-white font-bold py-3 border md:p-3 cursor-pointer ">
-                <a href="mailto:abc@gmail.com"> Learn more</a>
+                <a href="mailto:abc@gmail.com"> En savoir plus</a>
               </button>
             </div>
             <p className=" text-xl md:text-2xl font-light">
-              Equip your homes with our innovative solutions. Contribute to
-              protecting the environment and optimize the profitability of your
-              rental.
+              Equipez vos logements de nos solutions innovantes. Contribuez à la
+              protection de l'environnement et optimisez la rentabilité de votre
+              location.
+            </p>
+          </div>
+        </div>
+        <div className="flex flex-col items-center justify-center text-white space-y-4">
+          {/* Social Icons */}
+          <div className="flex space-x-10 md:space-x-24 mt-4 md:mt-5">
+            <a
+              href="https://facebook.com"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="hover:text-blue-500"
+            >
+              <FaFacebook size={35} />
+            </a>
+            <a
+              href="https://twitter.com"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="hover:text-blue-400"
+            >
+              <FaTwitter size={35} />
+            </a>
+            <a
+              href="https://linkedin.com"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="hover:text-blue-600"
+            >
+              <FaLinkedin size={35} />
+            </a>
+            <a
+              href="https://instagram.com"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="hover:text-pink-500"
+            >
+              <FaInstagram size={35} />
+            </a>
+          </div>
+          <div>
+            <p className="text-sm">
+              © {new Date().getFullYear()} ASERI. All rights reserved.
             </p>
           </div>
         </div>
